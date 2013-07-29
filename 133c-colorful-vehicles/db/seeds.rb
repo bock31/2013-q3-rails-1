fec5f8ce2fbbddbee0efd27b3ebaf9acb5313544
nicholas steele <nicholaspsteele@gmail.com>
2013-07-28 22:30:10 -0600

    completed.

--- 133c-colorful-vehicles/db/seeds.rb
+++ 133c-colorful-vehicles/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Color.create(name: "purple", html_color_code: "#6666FF")
+Color.create(name: "yellow", html_color_code: "#FFFF66")
+Color.create(name: "green", html_color_code: "#66FF66")
+Color.create(name: "fuschia", html_color_code: "#CC00FF")
+
+Vehicle.create(name: "bike", image_filename: "bike.png")
+Vehicle.create(name: "bus", image_filename: "bus.png")
+Vehicle.create(name: "car", image_filename: "car.png")
+Vehicle.create(name: "plane", image_filename: "plane.png")
+
+
+
+  
