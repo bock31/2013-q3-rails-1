79f311f43a080fffa21b307358d875f11263af37
nicholas steele <nicholaspsteele@gmail.com>
2013-07-30 09:59:12 -0600

    made hard-coded text dynamic, added get routes and input fields.

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -4,3 +4,15 @@ get "/" do
   @bracelet_lines = BraceletLine.order("id").all
   halt erb(:root)
 end
+
+get "/line1" do
+  @bracelet_line = BraceletLine.find(1)
+  halt erb(:show)
+end
+
+get "/line2" do
+  @bracelet_line = BraceletLine.find(2)
+  halt erb(:show)
+end
+
+
