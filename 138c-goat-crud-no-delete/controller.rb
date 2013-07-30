695fa4550c02b6f535350a406f16b46fde045d56
nicholas steele <nicholaspsteele@gmail.com>
2013-07-30 12:58:33 -0600

    added an "edit" erb page and routes

--- 138c-goat-crud-no-delete/controller.rb
+++ 138c-goat-crud-no-delete/controller.rb
@@ -1,2 +1,17 @@
 require '../dvc-sinatra.rb'
 
+get "/" do
+  @goats = RacingGoat.all
+  halt erb(:index)
+end
+
+get "/new-goat" do
+  halt erb(:new)
+end
+
+get "/edit/:id" do
+  id = params[:id]
+  @goat = RacingGoat.find(id)
+  halt erb(:edit)
+end
+
