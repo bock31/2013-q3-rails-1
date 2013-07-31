b93eed98e7491a193117b623e62968b7cb87a92b
nicholas steele <nicholaspsteele@gmail.com>
2013-07-31 11:27:36 -0600

    added POST handlers.

--- 134c-form-helpers/controller.rb
+++ 134c-form-helpers/controller.rb
@@ -3,3 +3,15 @@ require '../dvc-sinatra.rb'
 get "/" do
   halt erb(:root)
 end
+
+post "/" do
+  new_email         = Email.new
+  new_email.subject = params[:subject]
+  new_email.body     = params[:body]
+  new_email.save!
+  redirect "/"
+end
+
+# raise "variable".inspect (i.e. raise new_email.inspect 
+# or raise new_email.subject.inspect) will produced a "Runtime Error" 
+# and show you the value that has been assigned to the variable
