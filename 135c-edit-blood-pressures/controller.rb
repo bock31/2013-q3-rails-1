46714e7038d04570e977b9ce31d83e41a577714d
nicholas steele <nicholaspsteele@gmail.com>
2013-07-31 12:29:05 -0600

    added post handlers.

--- 135c-edit-blood-pressures/controller.rb
+++ 135c-edit-blood-pressures/controller.rb
@@ -10,3 +10,11 @@ get "/patients/:id" do
   @patient = Patient.find(id)
   halt erb(:edit)
 end
+
+post "/patients/:id" do
+  patient            = Patient.find(params[:id])
+  patient.systolic   = params[:systolic]
+  patient.diastolic  = params[:diastolic]
+  patient.save!
+  redirect "/"
+end
