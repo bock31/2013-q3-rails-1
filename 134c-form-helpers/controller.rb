548fc451522da4de97f359e5cbe24337d69868b4
nicholas steele <nicholaspsteele@gmail.com>
2013-07-31 14:12:06 -0600

    formatted.

--- 134c-form-helpers/controller.rb
+++ 134c-form-helpers/controller.rb
@@ -7,7 +7,7 @@ end
 post "/" do
   new_email         = Email.new
   new_email.subject = params[:subject]
-  new_email.body     = params[:body]
+  new_email.body    = params[:body]
   new_email.save!
   redirect "/"
 end
