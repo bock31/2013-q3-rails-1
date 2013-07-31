d16482948b2f8755a699f702a95d883e0ce9bcd3
nicholas steele <nicholaspsteele@gmail.com>
2013-07-31 17:36:29 -0600

    added post handler routes.

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -10,9 +10,23 @@ get "/line1" do
   halt erb(:edit)
 end
 
+post "/line1" do
+  line_edit           = BraceletLine.find(1)
+  line_edit.font_size = params[:font_size]
+  line_edit.text      = params[:text]
+  line_edit.save!
+  redirect "/"
+end
+
 get "/line2" do
   @bracelet_line = BraceletLine.find(2)
   halt erb(:edit)
 end
 
-
+post "/line2" do
+  line_edit           = BraceletLine.find(2)
+  line_edit.font_size = params[:font_size]
+  line_edit.text      = params[:text]
+  line_edit.save!
+  redirect "/"
+end
