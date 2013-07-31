8de2d69f8610b78845aa80c506f5de015f137f57
nicholas steele <nicholaspsteele@gmail.com>
2013-07-31 14:23:23 -0600

    added post handler routes.

--- 136c-add-apartments/controller.rb
+++ 136c-add-apartments/controller.rb
@@ -4,3 +4,13 @@ get "/" do
   @apartments = Apartment.all
   halt erb(:root)
 end
+
+post "/" do
+  new_apartment                 = Apartment.new
+  new_apartment.address         = params[:address]
+  new_apartment.rent_per_month  = params[:rent_per_month]
+  new_apartment.does_allow_pets = params[:does_allow_pets]
+  new_apartment.url             = params[:url]
+  new_apartment.save!
+  redirect "/"
+end  
