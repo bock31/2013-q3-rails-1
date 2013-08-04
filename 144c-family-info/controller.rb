a4e5ec4a7f120c57b00857ff7ca5cd7e15800f81
nicholas steele <nicholaspsteele@gmail.com>
2013-08-04 10:34:35 -0600

    added post handlers.

--- 144c-family-info/controller.rb
+++ 144c-family-info/controller.rb
@@ -6,5 +6,13 @@ get "/" do
 end
 
 post "/" do
-  # TODO: write this part
+  person                   = Person.new
+  person.relation_to_filer = params[:relation_to_filer_new]
+  person.first_name        = params[:first_name_new]
+  person.last_name         = params[:last_name_new]
+  person.date_of_birth     = params[:date_of_birth_new]
+  person.gender            = params[:gender_new]
+  person.is_dependent      = params[:is_dependent_new] == "on"
+  person.save!
+  redirect "/"
 end
