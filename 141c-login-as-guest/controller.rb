e0242866420223b5366fe8eebcfaa537332048ec
nicholas steele <nicholaspsteele@gmail.com>
2013-08-05 09:21:08 -0600

    added more comments.

--- 141c-login-as-guest/controller.rb
+++ 141c-login-as-guest/controller.rb
@@ -7,6 +7,8 @@ end
 post "/" do
 # alternate way to write the if statement below:
 # if params[:guest_login] == "on"
+# NOTE: your form helper in your erb must match 
+# i.e. "on" & "on" or "1" & "1"
   if params[:guest_login] != nil
     redirect "/welcome"
   end
