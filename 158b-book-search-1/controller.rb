adf94623c27c5fef8e4d6288188b02d158f773ad
nicholas steele <nicholaspsteele@gmail.com>
2013-08-09 15:50:01 -0600

    added get params.

--- 158b-book-search-1/controller.rb
+++ 158b-book-search-1/controller.rb
@@ -5,12 +5,19 @@ get "/" do
 end
 
 get "/year/:year" do
-  # TODO: Finish year route by
-  #       - assigning to @books
-  #       - assigning to @header
+  @books = Book.where(publication_year: params[:year]).all
+  @header = "Books published in #{params[:year]}"
   halt erb(:index)
 end
 
-# TODO: Write author route
+get "/topic/:topic" do
+  @books = Book.where("title ilike ?", "%#{params[:topic]}%").all
+  @header = "Books published about #{params[:topic]}"
+  halt erb(:index)
+end
 
-# TODO: Write topic route
+get "/author/:author" do
+  @books = Book.where("author ilike ?", "%#{params[:author]}%").all
+  @header = "Books published by #{params[:author]}"
+  halt erb(:index)
+end
