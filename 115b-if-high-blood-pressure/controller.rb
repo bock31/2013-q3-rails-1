cf7f27d40c4c348b5b10c5f3414c4bb107d65ce4
nicholas steele <nicholaspsteele@gmail.com>
2013-08-11 19:20:13 -0600

    added instance variables.

--- 115b-if-high-blood-pressure/controller.rb
+++ 115b-if-high-blood-pressure/controller.rb
@@ -4,4 +4,30 @@ get "/" do
   halt erb(:index)
 end
 
-# TODO add more routes here
+get "/patients/1" do
+  @systolic = 110
+  @diastolic = 75
+  
+  halt erb(:show)
+end
+
+get "/patients/2" do
+  @systolic = 120
+  @diastolic = 79
+  
+  halt erb(:show)
+end
+
+get "/patients/3" do
+  @systolic = 118
+  @diastolic = 82
+  
+  halt erb(:show)
+end
+
+get "/patients/4" do
+  @systolic = 140
+  @diastolic = 90
+  
+  halt erb(:show)
+end
