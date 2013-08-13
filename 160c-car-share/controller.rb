e25ec6c8f233394340a6f843d8bdd2431194383c
nicholas steele <nicholaspsteele@gmail.com>
2013-08-13 12:11:12 -0600

    added @password to post handler /login and an if statement to test whether it matched the password in the database. added @error messages for wrong password and username. created get /reservation/:id. created @memeber and @ reservation.

--- 160c-car-share/controller.rb
+++ 160c-car-share/controller.rb
@@ -12,16 +12,26 @@ end
 
 post "/login" do 
   @username = params[:username]
+  @password = params[:password]
   
   Member.all.each do |member|
-    if @username == member.username
-      redirect "/reservations/#{member.id}"
+    if @username == member.username 
+      if @password == member.password
+        redirect "/reservations/#{member.id}"
+      else
+        @error = "Wrong password"
+        halt erb(:login)
+      end
     end
   end
 
+  @error = "Unknown username"
   halt erb(:login)
 end
 
-get "/reservations/1" do
+get "/reservations/:id" do
+  @member = Member.find(params[:id])
+  @reservation = 
+    Car.where(reserving_member_id: @member.id).all
   halt erb(:reservations)
 end
