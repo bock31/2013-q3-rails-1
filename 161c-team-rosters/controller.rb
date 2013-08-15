039c0608fdc601e32efde68204abf967abd43df9
nicholas steele <nicholaspsteele@gmail.com>
2013-08-15 13:30:51 -0600

    completed.

--- 161c-team-rosters/controller.rb
+++ 161c-team-rosters/controller.rb
@@ -1,3 +1,39 @@
 require '../dvc-sinatra.rb'
 
-# TODO: write GET and POST handlers
+get "/" do 
+  @teams = Team.all
+  halt erb(:teams)
+end
+
+post "/" do
+  if params[:new_team] != ""
+    new_team = Team.new
+    new_team.color = params[:new_team]
+    new_team.save!
+  end
+  
+  redirect "/"
+end
+
+get "/team/:color" do
+  color = params[:color]
+  @team = Team.where(color: color).first
+  @members = Member.where(team_id: @team.id).all
+  halt erb(:members)
+end
+
+
+post "/team/:color" do
+  color = params[:color]
+  name  = params[:new_team_member]
+  team = Team.where(color: color).first
+  
+  if params[:new_team_member] != ""
+    new_member       = Member.new
+    new_member.name  = name
+    new_member.team_id = team.id
+    new_member.save!
+  end
+    
+  redirect "/team/#{color}"
+end
