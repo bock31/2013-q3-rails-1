747e9995bd1e906a6b06c56946afa0c92237122c
nicholas steele <nicholaspsteele@gmail.com>
2013-08-15 18:18:35 -0600

    completed.

--- 154c-battleship/controller.rb
+++ 154c-battleship/controller.rb
@@ -11,6 +11,26 @@ post "/" do
   @guesses = Guess.all
   @rows = [1, 2, 3, 4, 5]
   @cols = ["A", "B", "C", "D", "E"]
-
-  # TODO: write this part (see instructions)
+  
+  row = params[:row]
+  col = params[:col]
+  previous_guess = Guess.where(row: row, col: col).first
+  if previous_guess != nil
+    @error = "Already guessed that cell."
+    halt erb(:grid)
+  else
+    guess = Guess.new
+    guess.row = row
+    guess.col = col
+    
+    ship = Ship.where(col: col, row: row).first
+    if ship != nil
+      guess.was_hit = true
+    else 
+      guess.was_hit = false
+    end
+    
+    guess.save!
+  end
+  redirect "/"
 end
