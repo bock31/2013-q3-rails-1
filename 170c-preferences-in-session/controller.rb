59a1feb89d7552e7ea8990e888b06cb94bc7c8b5
nicholas steele <nicholaspsteele@gmail.com>
2013-08-19 15:23:10 -0600

    created session ids.

--- 170c-preferences-in-session/controller.rb
+++ 170c-preferences-in-session/controller.rb
@@ -1,16 +1,15 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
-  # TODO #3: Read each setting from the session
-  #          and store in instance variables.
-
+  @letter = session[:favorite_letter]
   halt erb(:main)
 end
 
 post "/" do
-  # TODO #2: Save each setting the user entered into the session.
-  #          Try raise session.inspect to see if it got saved.
-
+  session[:color]  = params[:favorite_color]  
+  session[:letter] = params[:favorite_letter]
+  raise params[:favorite_letter].inspect
+  session[:number] = params[:favorite_number]
   redirect "/"
 end
 
