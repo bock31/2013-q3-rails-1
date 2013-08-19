666004fc4a276c44a1a93a86e17f7dfec7a41424
nicholas steele <nicholaspsteele@gmail.com>
2013-08-19 15:17:43 -0600

    completed post handler.

--- 166c-sushi/controller.rb
+++ 166c-sushi/controller.rb
@@ -10,7 +10,27 @@ get "/place_order" do
 end
 
 post "/place_order" do
-  # TODO: Write this
+  if params[:commit] == "Place order"
+    new_order              = Order.new
+    new_order.total_price  = 0.00 
+    new_order.table_number = params[:table_number]  
+    new_order.save!
+
+    Dish.all.each do |dish|
+      quantity = params["quantity_for_dish_#{dish.id}"]
+      if quantity != ""
+        new_line_item            = LineItem.new
+        new_line_item.dish_id    = dish.id
+        new_line_item.quantity   = quantity
+        new_line_item.line_price = dish.price * quantity.to_i
+        new_line_item.order_id   = new_order.id
+        new_line_item.save!
+        
+        new_order.total_price = new_order.total_price + new_line_item.line_price
+      end
+    end
+    new_order.save!
+  end
 
   redirect "/"
 end
