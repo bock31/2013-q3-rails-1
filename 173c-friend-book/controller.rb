7cd709f121b45f4fe194185c4d20133c21cd83b4
nicholas steele <nicholaspsteele@gmail.com>
2013-08-22 20:11:48 -0600

    completed.

--- 173c-friend-book/controller.rb
+++ 173c-friend-book/controller.rb
@@ -9,15 +9,35 @@ get "/login" do
 end
 
 post "/login" do
-  # TODO: write this
+  user = Person.where(name: params[:name]).first
+  
+  if params[:password] == ""
+    redirect "/login"
+  elsif user.authenticate(params[:password]) != false
+    session[:person_id] = user.id
+    redirect "/followers"
+  else
+    halt erb(:login)
+  end
 end
 
 get "/followers" do
-  # TODO: write this
+  @person = Person.find(session[:person_id])
+  
+  halt erb(:followers)
 end
 
 post "/followers" do
-  # TODO: write this
+  @person = Person.find(session[:person_id])
+
+  if params[:commit] == "Follow"
+    following                = Following.new
+    following.from_person_id = @person.id
+    following.to_person_id   = Person.where(name: params[:person_to_follow]).first.id
+    following.save!
+  end
+  
+  redirect "/followers"
 end
 
 get "/logout" do
