d489a8137fa7cf867c3076dcff9b3c6be9ebbd9f
nicholas steele <nicholaspsteele@gmail.com>
2013-08-27 14:39:00 -0600

    started to convert 177c (Sinatra app) to Rails

---
+++ 181c-restaurant-rails/app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+  
+  def tables
+    redirect_to "/" and return
+  end
+  
+  
+end
--- 181c-restaurant-rails/db/seeds.rb
+++ 181c-restaurant-rails/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Entree.create! \
+    name: 'Chicken Marsala Sauteed with Mushroom and Marsala Wine',
+      price: 21.95
+Entree.create! \
+    name: 'Grilled Filet Mignon with Port Wine Reduction',
+      price: 29.95
+Entree.create! \
+    name: 'Seared Salmon Fillet with Oven Roasted Tomato and Leek Cream Sauce',
+      price: 24.95
+Entree.create! \
+    name: 'Lobster Ravioli',
+      price: 26.95
+Entree.create! \
+    name: 'Pasta Primavera',
+      price: 18.00
+
+Table.create! number_of_chairs: 2
+Table.create! number_of_chairs: 2
+Table.create! number_of_chairs: 2
+Table.create! number_of_chairs: 4
+Table.create! number_of_chairs: 6
+Table.create! number_of_chairs: 6
+Table.create! number_of_chairs: 4
