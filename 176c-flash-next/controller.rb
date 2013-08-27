5c102ae2400cbc68432f796173b8a5f5551cf90f
nicholas steele <nicholaspsteele@gmail.com>
2013-08-27 13:39:22 -0600

    created flash errors and success.

--- 176c-flash-next/controller.rb
+++ 176c-flash-next/controller.rb
@@ -12,12 +12,15 @@ post "/login" do
   named_user = User.where(username: params[:username]).first
 
   if named_user == nil 
-    # TODO: Redirect to /login with message "Unknown username"
+    flash[:error] = "Unknown username"
+    redirect "/login"
   elsif named_user.authenticate(params[:password]) == false
-    # TODO: Redirect to /login with message "Wrong password"
+    flash[:error] = "Wrong password"
+    redirect "/login"
   else
     session[:user_id] = named_user.id
-    # TODO: Redirect to /welcome with message "You have successfully logged in"
+    flash[:success] = "You have successfully logged in"
+    redirect "/welcome"
   end
 end
 
