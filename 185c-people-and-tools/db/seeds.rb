7bcdde2356c151d80c16cbfc72cee72c1e41cf9b
nicholas steele <nicholaspsteele@gmail.com>
2013-09-03 10:08:58 -0600

    created seeds.

--- 185c-people-and-tools/db/seeds.rb
+++ 185c-people-and-tools/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+person1 = Person.create! name: "Eve"
+person2 = Person.create! name: "Ed"
+person2 = Person.create! name: "Tony"
+
+Tool.create! name: "bandsaw",     person: person1
+Tool.create! name: "drill press", person: person1
+Tool.create! name: "lathe",       person: person2
+Tool.create! name: "planer",      person: nil
