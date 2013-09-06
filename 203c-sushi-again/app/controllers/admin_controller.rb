76454912ff9bcf5ece665b87b730f4bdff07fc7b
nicholas steele <nicholaspsteele@gmail.com>
2013-09-06 12:17:31 -0600

    completed.

---
+++ 203c-sushi-again/app/controllers/admin_controller.rb
@@ -0,0 +1,59 @@
+class AdminController < ApplicationController
+  before_filter except: ["login", "login_post", "logout"] do
+    if session[:admin_id] != nil
+      @admin = Admin.where(id: session[:admin_id]).first
+    else
+      flash[:error] = "You must be logged in to see that page."
+      session[:attempted_path] = request.path_info
+      redirect_to "/login" and return
+    end
+  end
+
+  def login
+    render :login and return
+  end
+  
+  def login_post
+    username = params[:username]
+    password = params[:password]
+    admin    = Admin.where(username: username).first
+    
+    if admin == nil
+      flash.now[:error] = "Unknown username"
+      render :login and return
+    elsif admin.authenticate(password) == false
+      flash.now[:error] = "Wrong password"
+      render :login and return   
+     else 
+      session[:admin_id] = admin.id
+      
+      attempted_path = session[:attempted_path]
+      if attempted_path != nil
+        session[:attempted_path] = nil
+        redirect_to attempted_path and return
+      else
+        redirect_to "/admin" and return
+      end
+    end      
+  end
+  
+  def home
+    render :admin and return
+  end
+  
+  
+  def dishes
+    @dishes = Dish.order(:id).all
+    render :dishes and return
+  end
+  
+  def orders
+    @orders = Order.order(:id).all
+    render :orders and return
+  end
+  
+  def logout
+    session.clear
+    redirect_to "/" and return
+  end
+end
