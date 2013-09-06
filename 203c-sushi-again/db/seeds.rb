68ae947abc5036d889c6dcf962e42d13c2f7b65b
nicholas steele <nicholaspsteele@gmail.com>
2013-09-06 12:17:14 -0600

    created seeds.

--- 203c-sushi-again/db/seeds.rb
+++ 203c-sushi-again/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dish.create(id: 1, name: 'Tuna',             price: 5.00)
+Dish.create(id: 2, name: 'Salmon',           price: 4.75)
+Dish.create(id: 3, name: 'Yellowtail',       price: 5.25)
+Dish.create(id: 4, name: 'Super White Tuna', price: 4.50)
+Dish.create(id: 5, name: 'Squid',            price: 4.50)
+Dish.create(id: 6, name: 'Mackerel',         price: 4.00)
+Dish.create(id: 7, name: 'Flying Fish Roe',  price: 4.25)
+
+Admin.create! username: "bob", password: "123", password_confirmation: "123"
+Admin.create! username: "sue", password: "234", password_confirmation: "234"
