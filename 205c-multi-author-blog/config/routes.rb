0e670eac3a5265738169205a82240e1f1fb06d42
nicholas steele <nicholaspsteele@gmail.com>
2013-09-07 14:27:07 -0600

    created get and post "/edit/:id" and "/new"

--- 205c-multi-author-blog/config/routes.rb
+++ 205c-multi-author-blog/config/routes.rb
@@ -3,4 +3,8 @@ App::Application.routes.draw do
   get  "/login"    => "main#login"
   post "/login"    => "main#login_post"
   get  "/logout"   => "main#logout"
+  get  "/edit/:id" => "main#edit"
+  post "/edit/:id" => "main#edit_post"  
+  get  "/new"      => "main#new"
+  post "/new"      => "main#new_post"
 end
